737e41d622134321d14e4c9db5ce50c196b15948
Tachi <nomuramtr75@gmail.com>
2022-05-30 09:41:50 +0700

    blank page untuk home,profile,jadwal pertandingan

---
+++ lib/ui/widgets/jadwal_pertandingan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JadwalPert extends StatefulWidget {
+  const JadwalPert({Key? key}) : super(key: key);
+
+  @override
+  State<JadwalPert> createState() => _JadwalPertState();
+}
+
+class _JadwalPertState extends State<JadwalPert> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
