ea40accebee87d38741a8015ed6c73cbdd912aab
Tachi <nomuramtr75@gmail.com>
2022-06-02 14:43:13 +0700

    page edit nomor

---
+++ lib/ui/pages/edit_nomor.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:gosports/shared/theme.dart';
+import 'package:gosports/ui/pages/profile.dart';
+import 'package:gosports/ui/widgets/submit_button.dart';
+import 'package:page_transition/page_transition.dart';
+
+class EditNomor extends StatefulWidget {
+  const EditNomor({Key? key}) : super(key: key);
+
+  @override
+  State<EditNomor> createState() => _EditNomorState();
+}
+
+class _EditNomorState extends State<EditNomor> {
+  late TextEditingController emailNamaControler;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    emailNamaControler = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: kWhiteColor,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: const Color(0xff292C31),
+        elevation: 10,
+        leading: IconButton(
+            onPressed: () async {
+              await Navigator.push(
+                context,
+                PageTransition(
+                  type: PageTransitionType.leftToRight,
+                  duration: const Duration(milliseconds: 500),
+                  reverseDuration: const Duration(milliseconds: 500),
+                  child: const Profile(),
+                ),
+              );
+            },
+            icon: Image.asset(
+              'assets/back_icon.png',
+              width: 15,
+            )),
+        title: Text(
+          'NOMOR HANDPHONE',
+          style: GoogleFonts.montserrat(
+            fontWeight: FontWeight.w700,
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 24,
+                    left: 24,
+                    top: 29,
+                  ),
+                  child: TextFormField(
+                    controller: emailNamaControler,
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      labelText: 'Nomor Handphone',
+                      labelStyle: GoogleFonts.lexendDeca(
+                        fontSize: 16,
+                        fontWeight: regular,
+                        color: const Color.fromARGB(151, 116, 113, 113),
+                      ),
+                      hintText: 'Masukkan nomor...',
+                      hintStyle: GoogleFonts.montserrat(
+                        fontSize: 16,
+                        fontWeight: regular,
+                        color: const Color.fromARGB(151, 116, 113, 113),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: kBlackColor,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: kBlackColor,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      filled: true,
+                      fillColor: kWhiteColor,
+                      contentPadding: const EdgeInsets.all(24),
+                    ),
+                    style: GoogleFonts.montserrat(
+                      fontSize: 14,
+                      color: kBlackColor,
+                      fontWeight: regular,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 550),
+                  child: SubmitButton(
+                    text: 'Submit',
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
