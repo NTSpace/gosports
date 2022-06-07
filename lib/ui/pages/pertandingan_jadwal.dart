b96ef9b1e9dcbe17d6c969141d1da8974de4e4d5
Tachi <nomuramtr75@gmail.com>
2022-06-07 20:51:05 +0700

    update jadwal

--- lib/ui/pages/pertandingan_jadwal.dart
+++ lib/ui/pages/pertandingan_jadwal.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:gosports/shared/theme.dart';
+import 'package:gosports/ui/widgets/cardlivematch.dart';
+import 'package:gosports/ui/widgets/cardmatch.dart';
+import 'package:gosports/ui/widgets/datematch.dart';
 import 'package:page_transition/page_transition.dart';
 
 class PertandinganJadwal extends StatefulWidget {
@@ -135,6 +138,53 @@ class _PertandinganJadwalState extends State<PertandinganJadwal> {
                 ),
               ],
             ),
+            const SizedBox(
+              height: 23,
+            ),
+            const DateCard(date: 'Hari Ini'),
+            MatchLiveCard(
+              status: 'Live',
+              logo1: 'assets/warriorsTim.png',
+              logo2: 'assets/grizzliesTim.png',
+              namatim1: 'WARRIORS',
+              namatim2: 'GRIZZLIES',
+              onClicked: () {},
+            ),
+            MatchLiveCard(
+              status: 'Live',
+              logo1: 'assets/bucksTim.png',
+              logo2: 'assets/lakersTim.png',
+              namatim1: 'BUCKS',
+              namatim2: 'LAKERS',
+              onClicked: () {},
+            ),
+            const SizedBox(
+              height: 17,
+            ),
+            const DateCard(
+              date: 'Minggu, 17 April 2022',
+            ),
+            MatchCard(
+                status: '11:20',
+                logo1: 'assets/rocketsTim.png',
+                logo2: 'assets/kingsTim.png',
+                namatim1: 'ROCKETS',
+                namatim2: 'KINGS',
+                onClicked: () {}),
+            MatchCard(
+                status: '14:30',
+                logo1: 'assets/miamiTim.png',
+                logo2: 'assets/broklynTim.png',
+                namatim1: 'MIAMI',
+                namatim2: 'BROKLYN',
+                onClicked: () {}),
+            MatchCard(
+                status: '18:30',
+                logo1: 'assets/utahTim.png',
+                logo2: 'assets/bullsTim.png',
+                namatim1: 'UTAH',
+                namatim2: 'BULLS',
+                onClicked: () {}),
           ],
         ),
       ),
